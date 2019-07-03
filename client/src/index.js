0a3c85caa1a0078cca3fe7259ef5e6499017de93
ArthurM0rgan <46456968+TonyPrpich@users.noreply.github.com>
2019-07-02 20:48:40 -0600

    added new scss file

--- client/src/index.js
+++ client/src/index.js
@@ -2,5 +2,9 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
+import 'bootstrap/dist/css/bootstrap.css';
+import './custom.scss';
+
+
 
 ReactDOM.render(<App />, document.getElementById("root"));
