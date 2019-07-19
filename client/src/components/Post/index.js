d9d20356ad4b4b899cd952a8a5b1d6a47ced756d
Andrea Yoshida <aryoshida9340@gmail.com>
2019-07-18 21:24:03 -0600

    added an update modal and made it reload the discussion component on close

--- client/src/components/Post/index.js
+++ client/src/components/Post/index.js
@@ -3,8 +3,10 @@ import React from "react";
 import { Card } from 'react-bootstrap';
 import { Button } from 'react-bootstrap';
 import "./Post.css";
+import PostUpdateModal from "../PostUpdateModal";
 // import { Feed } from "semantic-ui-react";
 const axios = require("axios");
+
 
 export default class Post extends React.Component {
   constructor(props) {
@@ -40,21 +42,12 @@ export default class Post extends React.Component {
       return;
     }
 
-
-    function commentClick(event){
-      event.preventDefault();
-      console.log("Comment button clicked");
-      //pull up a modal to leave a comment
-      //it will need a body area and a submit button
-    }
-
-    function editClick(event){
-      event.preventDefault();
-      console.log("Edit button clicked")
-      //possibly pulls up a modal like the write post form
-      //2nd option, update the clicked on post
-      //1st option, delete old post and when new post submited it will be a new data entry
-    }
+    // function commentClick(event){
+    //   event.preventDefault();
+    //   console.log("Comment button clicked");
+    //   //pull up a modal to leave a comment
+    //   //it will need a body area and a submit button
+    // }
     
     return (
 
@@ -68,8 +61,12 @@ export default class Post extends React.Component {
           <Card.Text>{this.props.data.body}</Card.Text>
         </Card.Body>
         <Card.Footer>
-          <Button data-id={this.props.data._id} onClick={commentClick} className="post-comment-button" variant="outline-info" size="sm">Comment</Button>
-          <Button data-id={this.props.data._id} onClick={editClick} className="post-edit-button" variant="outline-secondary" size="sm">Edit</Button>
+          {/* <Button data-id={this.props.data._id} onClick={commentClick} className="post-comment-button" variant="outline-info" size="sm">Comment</Button> */}
+          <PostUpdateModal
+            loadData={this.props.loadData}
+            postId={this.props.data._id}
+          />
+          {/* <Button data-id={this.props.data._id} onClick={editClick} className="post-edit-button" variant="outline-secondary" size="sm">Edit</Button> */}
           <Button id={"delete-btn-"+this.props.data._id} onClick={this.deleteClick} className="post-delete-button" variant="outline-danger" size="sm">Delete</Button>
         </Card.Footer>
       </Card>
