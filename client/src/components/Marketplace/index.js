652c5e43c84e0654392560c3201a74d1f1e422b7
Jaron Johnson <jjaronjohnson14@gmail.com>
2019-07-20 09:55:21 -0600

    fixed merges

--- client/src/components/Marketplace/index.js
+++ client/src/components/Marketplace/index.js
@@ -82,7 +82,6 @@ class Marketplace extends Component {
                         {
                             // this.state.savedMarketItems
                             this.state.items.map(e =>
-<<<<<<< HEAD
                                 <MarketItem
                                     key={e._id}
                                     userId={e.userId}
@@ -95,30 +94,11 @@ class Marketplace extends Component {
                                 // contactEmail={e.contactEmail}
 
                                 />
-=======
-                                <>
-                                    <MarketItem
-                                        userId={e.userId}
-                                        title={e.title}
-                                        description={e.description}
-                                        price={e.price}
-                                        contactPhone={e.contactPhone}
-                                        imageName={e.imageName}
-                                        imageData={e.imageData}
-                                    // contactEmail={e.contactEmail}
-
-                                    />
-                                </>
->>>>>>> 5b94a6601e11c5a22dfaf10e58b4d6390bb2a6b6
                             )
                         }
                     </Card.Content>
                 </Card>
-<<<<<<< HEAD
-            </div >
-=======
             </div>
->>>>>>> 5b94a6601e11c5a22dfaf10e58b4d6390bb2a6b6
         )
     }
 }
